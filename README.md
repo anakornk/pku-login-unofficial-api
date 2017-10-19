a814efc73e5b0e6f7b355f5e405eec99ff9c19fa
Anakorn Kyavatanakij <anakornk@gmail.com>
2017-10-20 01:08:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PKU Login
+
---
+++ index.js
@@ -0,0 +1,37 @@
+var request = require("request");
+function check(userName,password){
+  var options = {
+    method: 'POST',
+    url: 'https://iaaa.pku.edu.cn/iaaa/oauthlogin.do',
+    headers: { 'postman-token': '5e3cb6eb-34ad-b2c0-9d95-058c1e9750fa',
+       'cache-control': 'no-cache',
+       'content-type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+      appid: 'blackboard',
+      userName,
+      password,
+      redirUrl: 'http://course.pku.edu.cn/webapps/bb-sso-bb_bb60/execute/authValidate/campusLogin'
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    var res = JSON.parse(body);
+    if(res.success){
+      console.log("登录成功")
+    }else{
+      console.log(res.errors.msg);
+    }
+  });
+}
+
+
+if(process.argv.length < 4) {
+  console.log("Usage: node index.js username password");
+} else {
+  let username = process.argv[2];
+  let password = process.argv[3];
+  check(username,password);
+}
+
